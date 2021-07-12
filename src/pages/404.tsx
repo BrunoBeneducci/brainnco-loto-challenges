ac71469a29f0775d4c6e266a3fd76ac3ded8cf48
BrunoBeneducci <beneducci@gmail.com>
2021-07-12 15:06:50 -0300

    pagina 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import FullLoader from '../components/FullLoader';
+import imgLoterias from '../images/logo-loterias.svg';
+import Api from '../services/api';
+import { useSelectGame } from '../context/SelectGame';
+import './pages.scss';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
 
+    const {valueGame, nameGame} = useSelectGame();
+    const [loader, setLoader] = useState(true);
+
+    useEffect(() => {
+        setLoader(true);
+
+        Api.get('/loterias-concursos')
+        .then(({ data }) => {
+            return data[valueGame].concursoId;
+        })
+        .then(() => {
+            setTimeout(() => {
+                setLoader(false);
+            }, 300);
+        })
+        .catch(() => { console.log('Ocorreu algum erro :(') })
+    }, [valueGame, nameGame]);
+
     return (
-        <div className="not-found">
-            <h1>404</h1>
+        <div className={nameGame ? nameGame.split(" ").join("-").toLowerCase() + ' page' : 'page'}>
+
+            <FullLoader status={loader}/>
+                
+            <div className="page-box page-box-404">
+
+                <div className="page-box-title">
+                    <img src={imgLoterias} alt="Logo Loteriais" />
+                    <h1>Ops, página não encontrada :(</h1>
+                    <p>A página que procurou não existe ou não está mais disponível.</p>
+                    <Link to="/">Voltar para home</Link>
+                </div>
+            </div>
+
         </div>
     )
 }
